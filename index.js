143d93e46e6255283f73012134024bc514794bc8
James Turle <james@foyerlive.com>
2018-08-12 13:02:29 -0400

    initial connection testing

---
+++ index.js
@@ -0,0 +1,19 @@
+var linkup = require("linkup");
+
+// create a peer with some id
+var peer = linkup.createPeer("peer1");
+
+// listen for messages from other peers
+peer.on("message", function(envelope) {
+  console.log("Received message from", envelope.from, ":", envelope.message);
+});
+
+// we want to know when something goes wrong
+peer.on("error", function(err) {
+  console.error(err);
+});
+
+// send a message to a peer
+peer.send("peer2", "hi peer2!").catch(function(err) {
+  console.error(err);
+});
